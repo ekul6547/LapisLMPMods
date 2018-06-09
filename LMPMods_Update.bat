bbbb1bfa7d9ab3f3f96144e92b83ac867ae96611
ReLapis <ekul.lamb@live.co.uk>
2018-06-09 18:04:06 +0100

    Update LMPMods_Update.bat

--- LMPMods_Update.bat
+++ LMPMods_Update.bat
@@ -7,6 +7,6 @@ call git remote add origin https://github.com/ekul6547/LapisLMPMods.git
 call git pull origin master
 
 
-echo Finished. If process aborted, please contact Luke.
+echo Finished. If process aborted, please contact Me.
 pause
 exit
