be4b7c778b3837dfae68ac4d0fc19b614e74e42e
ekul6547 <ekul.lamb@live.co.uk>
2018-06-13 17:19:37 +0100

    Bat edit 1

--- Bat Files/LMPMods_Update.bat
+++ Bat Files/LMPMods_Update.bat
@@ -1,5 +1,6 @@
 @ECHO OFF
 
+IF EXIST GameData\(
 IF NOT EXIST .git\ (
 call git init
 call git remote add origin https://github.com/ekul6547/LapisLMPMods.git
@@ -8,5 +9,8 @@ call git pull origin master
 
 
 echo Finished. If process aborted, please contact Me.
+) ELSE (
+echo Bat placed in incorrect directory.
+)
 pause
 exit
